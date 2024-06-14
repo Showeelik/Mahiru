236fe09718a34df951dc938ba4bae976a7f67ba1
Showeelik <zeezxex2000@yandex.ru>
2024-06-14 21:02:44 +0300

    Созданы шаблоны для Функции логов

---
+++ src/loggers.py
@@ -0,0 +1,17 @@
+import logging
+
+
+class CustomFormatter(logging.Formatter):
+    """
+    Custom formatter for logging
+    """
+    pass
+
+
+def setup_logger(module_name: str) -> logging.Logger:
+    """
+    Setup logger for module
+    :param module_name: module name
+    :return: logger
+    """
+    pass
